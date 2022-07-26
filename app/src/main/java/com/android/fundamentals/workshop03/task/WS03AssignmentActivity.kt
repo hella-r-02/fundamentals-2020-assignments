e620fd4d263ca1f37765ada97e2aa0c104c5c59e
hella-r-02 <77581078+hella-r-02@users.noreply.github.com>
2022-07-26 12:17:10 +0300

    Add ex-3 workshop3

--- app/src/main/java/com/android/fundamentals/workshop03/task/WS03AssignmentActivity.kt
+++ app/src/main/java/com/android/fundamentals/workshop03/task/WS03AssignmentActivity.kt
@@ -5,11 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import com.android.fundamentals.R
 import com.android.fundamentals.workshop03.WS03SecondFragment
 
-//TODO(WS2:9) Implement interface in Activity
-class WS03AssignmentActivity : AppCompatActivity() {
-
-    //TODO(WS2:9) Create root fragment and set listener
+class WS03AssignmentActivity : AppCompatActivity(), WS03AssignmentFragment.ClickListener {
     private val secondFragment = WS03SecondFragment()
+    private val rootFragment =
+        WS03AssignmentFragment().apply { setListener(this@WS03AssignmentActivity) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,14 +20,18 @@ class WS03AssignmentActivity : AppCompatActivity() {
 
 
         fragmentTransaction.apply {
-            //TODO(WS2:3) Add the Fragment to the R.id.persistent_container FrameLayout
-            //add(R.id.persistent_container, rootFragment)
+            add(R.id.persistent_container, rootFragment)
             add(R.id.fragments_container, secondFragment)
             commit()
         }
     }
 
-    //TODO(WS2:10) Change the text in secondFragment
-    //TODO(WS2:11) Change fragment text background in secondFragment
+    override fun increaseValue() {
+        secondFragment.increaseValue()
+    }
+
+    override fun changeBackground() {
+        secondFragment.changeBackground()
+    }
 
 }
--- app/src/main/java/com/android/fundamentals/workshop03/task/WS03AssignmentFragment.kt
+++ app/src/main/java/com/android/fundamentals/workshop03/task/WS03AssignmentFragment.kt
@@ -8,14 +8,12 @@ import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.android.fundamentals.R
 
-//TODO(WS2:1) Create fragment class, extend Fragment
 class WS03AssignmentFragment : Fragment() {
 
     private var btnIncrement: Button? = null
     private var btnChangeBackground: Button? = null
-    //TODO(WS2:5) Create a variable ClickListener
+    private var clickListener: ClickListener? = null
 
-    //TODO(WS2:2) Override onCreateView() method
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -25,15 +23,24 @@ class WS03AssignmentFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         btnIncrement = view.findViewById<Button>(R.id.btn_increment).apply {
-            //TODO(WS2:7) Set button OnClickListener
+            setOnClickListener {
+                clickListener?.increaseValue()
+            }
         }
         btnChangeBackground = view.findViewById<Button>(R.id.btn_change_background).apply {
-            //TODO(WS2:8) Set button OnClickListener
+            setOnClickListener {
+                clickListener?.changeBackground()
+            }
         }
 
     }
 
-    //TODO(WS2:6) Create fun setListener() initialize clickListener in Fragment
+    fun setListener(l: ClickListener) {
+        clickListener = l
+    }
 
-    //TODO(WS2:4) Create interface ClickListener
+    interface ClickListener {
+        fun increaseValue()
+        fun changeBackground()
+    }
 }
