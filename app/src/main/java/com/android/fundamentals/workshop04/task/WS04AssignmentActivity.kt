c78bdaf33854d4156700a1e680851c8c8f342d51
hella-r-02 <77581078+hella-r-02@users.noreply.github.com>
2022-07-26 17:56:29 +0300

    Add ex-3 workshop4

--- app/src/main/java/com/android/fundamentals/workshop04/task/WS04AssignmentActivity.kt
+++ app/src/main/java/com/android/fundamentals/workshop04/task/WS04AssignmentActivity.kt
@@ -1,12 +1,16 @@
 package com.android.fundamentals.workshop04.task
 
+import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.android.fundamentals.R
+import com.android.fundamentals.workshop04.SampleBottomSheet
 import com.google.android.material.snackbar.Snackbar
+import java.util.*
 
 class WS04AssignmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,31 +19,62 @@ class WS04AssignmentActivity : AppCompatActivity() {
 
         findViewById<Button>(R.id.btn_show_alert_dialog)?.apply {
             setOnClickListener {
-                // TODO(WS4:1) set title, positive, negative and neutral buttons for AlertDialog.
-                //  Make it show Toast when pressing button and show in toast what button was pressed
-                //  * Make it show Toast about cancelling dialog only when it is closed by tapping outside
-                //  * Make dialog not to be closed when tapped outside of fragment
-                //  AlertDialog.Builder(context)
-                //  .show()
+
+                AlertDialog.Builder(context)
+                    .setTitle("Alert!!!")
+                    .setPositiveButton("ok") { _, _ ->
+                        Toast.makeText(
+                            context,
+                            "ok",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    .setNegativeButton("cancel") { _, _ ->
+                        Toast.makeText(
+                            context,
+                            "cancel",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    .setNeutralButton("retry") { _, _ ->
+                        Toast.makeText(
+                            context,
+                            "retry",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    .setOnCancelListener {
+                        Toast.makeText(
+                            context,
+                            "thw window is closed",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    .show()
             }
         }
 
         findViewById<Button>(R.id.btn_show_dialog_fragment)?.apply {
             setOnClickListener {
-                // TODO(WS4:2) show dialog fragment SampleDialogFragment.
-                //  Change SampleDialogFragment to make it show Toasts as in alert dialog (previous task)
+                val dialog = WS04AssignmentDialogFragment()
+                dialog.show(supportFragmentManager, "dialog fragment")
             }
         }
 
         findViewById<Button>(R.id.btn_show_time_picker)?.apply {
             setOnClickListener {
-                // TODO(WS4:3) make timePickerDialog to start with current time
-                //  Show Snackbar with selected time
+                val hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+                val minute = Calendar.getInstance().get(Calendar.MINUTE)
                 val timePickerDialog = TimePickerDialog(
                     this@WS04AssignmentActivity,
-                    { p1, p2, p3 -> },
-                    0,
-                    0,
+                    { p1, p2, p3 ->
+                        Snackbar
+                            .make(rootView, "$p2:$p3", Snackbar.LENGTH_SHORT)
+                            .show()
+                        println("$p2:$p3")
+                    },
+                    hour,
+                    minute,
                     true
                 )
 
@@ -49,21 +84,22 @@ class WS04AssignmentActivity : AppCompatActivity() {
 
         findViewById<Button>(R.id.btn_show_date_picker)?.apply {
             setOnClickListener {
-                // TODO(WS4:4) make timePickerDialog to start with today date
-                //  Show Snackbar with selected datek
+                val year = Calendar.getInstance().get(Calendar.YEAR)
+                val month = Calendar.getInstance().get(Calendar.MONTH)
+                val dayOfMonth = Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
                 val datePickerDialog = DatePickerDialog(
                     this@WS04AssignmentActivity,
                     { p0, p1, p2, p3 ->
                         Snackbar.make(
                             rootView,
-                            "you choosed $p3/$p2/$p1",
+                            "you chose $p3/$p2/$p1",
                             Snackbar.LENGTH_LONG
                         )
                             .show()
                     },
-                    0,
-                    0,
-                    0
+                    year,
+                    month,
+                    dayOfMonth
                 )
 
                 datePickerDialog.show()
@@ -72,8 +108,7 @@ class WS04AssignmentActivity : AppCompatActivity() {
 
         findViewById<Button>(R.id.btn_show_bottom_sheet_dialog)?.apply {
             setOnClickListener {
-                // TODO(WS4:5) show dialog fragment SampleBottomSheet
-                //  Look at difference between dialogFragment and BottomSheetFragment layouts drawing  and change dialog_fragment to show buttons under textview
+                SampleBottomSheet().show(supportFragmentManager, "bottom sheet dialog")
             }
         }
     }
--- app/src/main/java/com/android/fundamentals/workshop04/task/WS04AssignmentDialogFragment.kt
+++ app/src/main/java/com/android/fundamentals/workshop04/task/WS04AssignmentDialogFragment.kt
@@ -1,9 +1,12 @@
 package com.android.fundamentals.workshop04.task
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import com.android.fundamentals.R
 
@@ -17,10 +20,41 @@ class WS04AssignmentDialogFragment : DialogFragment() {
         return inflater.inflate(R.layout.dialog_fragment_ws04, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.btn_ok).setOnClickListener {
+            Toast.makeText(
+                context,
+                "ok",
+                Toast.LENGTH_SHORT
+            ).show()
+            dismiss()
+        }
+        view.findViewById<Button>(R.id.btn_cancel)?.setOnClickListener {
+            Toast.makeText(
+                context,
+                "cancel",
+                Toast.LENGTH_SHORT
+            ).show()
+            dismiss()
+        }
+
+    }
+
     override fun onStart() {
         super.onStart()
         val width = (resources.displayMetrics.widthPixels * 0.85).toInt()
         val height = (resources.displayMetrics.heightPixels * 0.40).toInt()
         dialog?.window?.setLayout(width, height)
     }
+
+    override fun onCancel(dialog: DialogInterface) {
+        Toast.makeText(
+            context,
+            "thw window is closed",
+            Toast.LENGTH_SHORT
+        ).show()
+        super.onCancel(dialog)
+    }
+
 }
