f8fece6b90fa2f436d32e24207798c51871f8a38
hella-r-02 <77581078+hella-r-02@users.noreply.github.com>
2022-07-29 22:32:41 +0300

    add ex-5 workshop1

--- app/src/main/java/com/android/academy/fundamentals/workshop_1/WS01ThreadsTaskFragment.kt
+++ app/src/main/java/com/android/academy/fundamentals/workshop_1/WS01ThreadsTaskFragment.kt
@@ -1,18 +1,22 @@
 package com.android.academy.fundamentals.workshop_1
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.android.academy.fundamentals.R
 
-class WS01ThreadsTaskFragment: Fragment(R.layout.fragment_ws_01) {
+class WS01ThreadsTaskFragment : Fragment(R.layout.fragment_ws_01) {
 
-    private var threadButton : Button? = null
-    private var threadTextView : TextView? = null
+    private var threadButton: Button? = null
+    private var threadTextView: TextView? = null
 
-    //TODO(WS1:2) Create a private val handler and set yours
+    private val handler = MyHandler()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         findViews(view)
@@ -35,40 +39,48 @@ class WS01ThreadsTaskFragment: Fragment(R.layout.fragment_ws_01) {
 
     private fun startThread() {
         printMessage(getString(R.string.wait))
-        //TODO(WS1:5) create your thread and start it
+        val thread = MyThread()
+        thread.start()
     }
 
     private fun startRunnable() {
         printMessage(getString(R.string.wait))
-        //TODO(WS1:8) create your runnable and start it
+        val runnable = MyRunnable()
+        runnable.run()
     }
 
-    private fun printMessage(mes: String){
-        threadTextView?.text =  mes
+    private fun printMessage(mes: String) {
+        threadTextView?.text = mes
     }
 
-    //TODO(WS1:1) Create inner class Handler
-    // example: MyHandler : Handler(Looper.getMainLooper())
+    @SuppressLint("HandlerLeak")
+    inner class MyHandler : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+            printMessage(msg.data.getString(MESSAGE_KEY, ""))
+        }
+    }
 
-    //TODO(WS1:2) override function handleMessage(mes: Message)
-    // at the function run printMessage
-    // Get string from mes.data.getString(MESSAGE_KEY, "")
+    inner class MyThread : Thread() {
+        override fun run() {
+            sleep(6000)
+            val msg = Message()
+            msg.data.putString(MESSAGE_KEY, getString(R.string.thread_worked))
+            handler.sendMessage(msg)
 
-    //TODO(WS1:3) Create inner class Thread
-    // example: MyThread : Thread()
-    // override function run
+        }
+    }
 
-    //TODO(WS1:4) at the run function emulate long work with sleep(6000)
-    // create Message, put string to the message.data with MESSAGE_KEY
-    // send message to the handler handler.sendMessage(mes)
+    inner class MyRunnable : Runnable {
+        override fun run() {
+            Thread.sleep(4000)
+            val msg = Message()
+            msg.data.putString(MESSAGE_KEY, getString(R.string.runnable_worked))
+            handler.sendMessage(msg)
 
-    //TODO(WS1:6) Create inner class Runnable
-    // example:  MyRunnable : Runnable
-    // override function run
+        }
 
-    //TODO(WS1:7) at the run function emulate long work with Thread.sleep(4000)
-    // create Message, put string to the message.data with MESSAGE_KEY
-    // send message to the handler handler.sendMessage(mes)
+    }
 
     companion object {
         private const val MESSAGE_KEY = "key"
