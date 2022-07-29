a5130df75c0bae4518f79808b710abc0a8a47893
hella-r-02 <77581078+hella-r-02@users.noreply.github.com>
2022-07-30 00:35:08 +0300

    add ex-5 workshop2

--- app/src/main/java/com/android/academy/fundamentals/MainActivity.kt
+++ app/src/main/java/com/android/academy/fundamentals/MainActivity.kt
@@ -3,6 +3,7 @@ package com.android.academy.fundamentals
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.android.academy.fundamentals.workshop_1.WS01ThreadsTaskFragment
+import com.android.academy.fundamentals.workshop_2.WS02FirstCoroutineTaskFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .addToBackStack(null)
-                .replace(R.id.fragment_container_view, WS01ThreadsTaskFragment())
+                .replace(R.id.fragment_container_view, WS02FirstCoroutineTaskFragment())
                 .commit()
         }
     }
--- app/src/main/java/com/android/academy/fundamentals/workshop_2/WS02FirstCoroutineTaskFragment.kt
+++ app/src/main/java/com/android/academy/fundamentals/workshop_2/WS02FirstCoroutineTaskFragment.kt
@@ -8,12 +8,14 @@ import android.widget.ScrollView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.android.academy.fundamentals.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class WS02FirstCoroutineTaskFragment: Fragment(R.layout.fragment_ws_02) {
+class WS02FirstCoroutineTaskFragment : Fragment(R.layout.fragment_ws_02) {
 
-    // TODO(WS2:1) create scope (CoroutineScope) for future coroutines with context Dispatchers.Main
-    // private val scope = ...
-
+    private val scope = CoroutineScope(Dispatchers.Main)
     private var textView: TextView? = null;
     private var button: Button? = null;
     private var scrollView: ScrollView? = null;
@@ -26,35 +28,24 @@ class WS02FirstCoroutineTaskFragment: Fragment(R.layout.fragment_ws_02) {
         scrollView = view.findViewById(R.id.scrollView)
 
         button?.setOnClickListener {
-            // TODO(WS2:2) create coroutine - call readFromFile from scope using launch and launch the app
+            scope.launch {
+                readFromFile()
+            }
         }
     }
 
-    // TODO(WS2:3) reading from file is a heavy work, it should not be done from the main thread
-    //  let's add a context - Dispatcher to readFromFile
-
-    // TODO(WS2:4) make readFromFile suspended.
-    //  Add a keyword - suspend - to readFromFile function.
-    //  Then launch the app
-    private fun readFromFile() {
-        val file = context?.resources?.openRawResource(R.raw.alice);
+    private suspend fun readFromFile() = withContext(Dispatchers.IO) {
+        val file = context?.resources?.openRawResource(R.raw.alice)
         file?.bufferedReader()
-                ?.useLines { lines ->
-                    lines.forEach {
-                        updateTextView(it)
-                    }
+            ?.useLines { lines ->
+                lines.forEach {
+                    updateTextView(it)
                 }
+            }
     }
 
-    // TODO(WS2:5) the app crashes because textView is updated not from the main thread (UI thread)
-    //  you need to add context - Dispatchers.Main - and suspend to updateTextView so the view is updated from the main thread
-    //  launch the app and check if the app works correctly
-    private fun updateTextView(text: String) {
+    private suspend fun updateTextView(text: String) = withContext(Dispatchers.Main) {
         textView?.append("\n$text")
         scrollView?.fullScroll(View.FOCUS_DOWN)
     }
-
-    // TODO(WS2:6)* update class variable 'scope' from Dispatchers.Main to Dispatchers.Default
-    //  then think and discuss with the team and your mentor what context readFromFile
-    //  and updateTextView should have after the change
 }
