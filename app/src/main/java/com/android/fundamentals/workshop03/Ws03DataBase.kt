67b5201c3756eb39559637c7ec7ad4efa4f55746
hella-r-02 <77581078+hella-r-02@users.noreply.github.com>
2022-08-11 17:51:24 +0300

    add ex-8 workshop3

--- app/src/main/java/com/android/fundamentals/workshop03/Ws03DataBase.kt
+++ app/src/main/java/com/android/fundamentals/workshop03/Ws03DataBase.kt
@@ -5,20 +5,28 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.android.fundamentals.workshop03.Ws03DbContract
+import com.android.fundamentals.workshop03.antibonus.Ws03BonusViewModel
+import com.android.fundamentals.workshop03.solution.Ws03SolutionDataBase
 
 // TODO 10: Make this class abstract and extend from "RoomDatabase()".
 //  Mark with @Database() annotation with "entities = [Ws03LocationEntity::class], version = 1" as params.
-class Ws03DataBase {
+@Database(entities = [Ws03LocationEntity::class], version = 1)
+abstract class Ws03DataBase : RoomDatabase() {
 
-	// TODO 11: Listed all DAOs as abstract properties (we have only one "Ws03LocationsDao").
-	
-	companion object {
-		
-		// TODO 12: Add a function to create a DataBase with "application: Context" as param.
-		//  It returns an instance of "Ws03DataBase".
-		//  To create a DB, we have to create "Room.databaseBuilder" with params:
-		//  (@NonNull Context context, @NonNull Class<T> klass, @NonNull String dbName).
-		//  Provide "applicationContext", "Ws03DataBase::class.java" and "DATABASE_NAME" from the "Ws03DbContract".
-		//  Add ".fallbackToDestructiveMigration()", build() the Builder.
-	}
+    // TODO 11: Listed all DAOs as abstract properties (we have only one "Ws03LocationsDao").
+    abstract val locationsDao: Ws03LocationsDao
+
+    companion object {
+
+        // TODO 12: Add a function to create a DataBase with "application: Context" as param.
+        //  It returns an instance of "Ws03DataBase".
+        //  To create a DB, we have to create "Room.databaseBuilder" with params:
+        //  (@NonNull Context context, @NonNull Class<T> klass, @NonNull String dbName).
+        //  Provide "applicationContext", "Ws03DataBase::class.java" and "DATABASE_NAME" from the "Ws03DbContract".
+        //  Add ".fallbackToDestructiveMigration()", build() the Builder.
+        fun create(context: Context): Ws03DataBase =
+            Room.databaseBuilder(context, Ws03DataBase::class.java, Ws03DbContract.DATABASE_NAME)
+                .fallbackToDestructiveMigration()
+                .build()
+    }
 }
--- app/src/main/java/com/android/fundamentals/workshop03/Ws03LocationsRepository.kt
+++ app/src/main/java/com/android/fundamentals/workshop03/Ws03LocationsRepository.kt
@@ -15,52 +15,66 @@ import kotlin.random.Random
 //  * getAllLocations(): List<Location>					- select all locations from DB;
 //  * addNewAndGetUpdated(): List<Location>				- insert one location and select all;
 //  * deleteByIdAndGetUpdated(id: Long): List<Location>	- delete one location and select all;
-class Ws03LocationsRepository(applicationContext: Context) {
-	
-	private val random = Random(10)
-	
-	// TODO 13: Create a property with type of the "Ws03DataBase" and initialize here.
-	//  Use function from "TODO_12".
-	
-	// TODO 14: override suspend fun getAllLocations().
-	//  Call this function "withContext(Dispatchers.IO)".
-	//  Call LocationDao "getAll()" and use "toLocation(...)" to "map" list with Entities to list with Locations.
-	
-	// TODO 15: override suspend fun addNewAndGetUpdated().
-	//  Call this function "withContext(Dispatchers.IO)".
-	//  Create "NewLocationRequest" from "generateNewLocationRequest()".
-	//  Convert "NewLocationRequest" to " Ws03LocationEntity" with "toEntity(...)".
-	//  Pass the entity to the LocationDao "insert(...)".
-	//  Call "getAllLocations()" in the end.
-	
-	// TODO 16: override suspend fun deleteByIdAndGetUpdated(id: Long).
-	//  Call this function "withContext(Dispatchers.IO)".
-	//  Call LocationDao "deleteById(id)" to delete location entry.
-	//  Call "getAllLocations()" in the end.
+class Ws03LocationsRepository(applicationContext: Context) : LocationRepository {
 
-	private fun toEntity(location: NewLocationRequest) = Ws03LocationEntity(
-		title = location.title,
-		lat = location.latitude,
-		lon = location.longitude
-	)
+    private val random = Random(10)
 
-	private fun toLocation(entity: Ws03LocationEntity) = Location(
-		id = entity.id,
-		title = "${entity.title}, id:${entity.id}",
-		latitude = entity.lat,
-		longitude = entity.lon
-	)
+    // TODO 13: Create a property with type of the "Ws03DataBase" and initialize here.
+    //  Use function from "TODO_12".
+    private val dataBase = Ws03DataBase.create(context = applicationContext)
 
-	private suspend fun generateNewLocationRequest() = withContext(Dispatchers.Default) {
-		delay(DELAY_MILLIS)
-		NewLocationRequest(
-			title = "Title ${random.nextInt()}",
-			latitude = random.nextDouble(180.0),
-			longitude = random.nextDouble(180.0)
-		)
-	}
+    // TODO 14: override suspend fun getAllLocations().
+    //  Call this function "withContext(Dispatchers.IO)".
+    //  Call LocationDao "getAll()" and use "toLocation(...)" to "map" list with Entities to list with Locations.
+    override suspend fun getAllLocations(): List<Location> = withContext(context = Dispatchers.IO) {
+        dataBase.locationsDao.getAllLocations().map { toLocation(it) }
+    }
 
-	companion object {
-		private const val DELAY_MILLIS: Long = 1_000
-	}
+    // TODO 15: override suspend fun addNewAndGetUpdated().
+    //  Call this function "withContext(Dispatchers.IO)".
+    //  Create "NewLocationRequest" from "generateNewLocationRequest()".
+    //  Convert "NewLocationRequest" to " Ws03LocationEntity" with "toEntity(...)".
+    //  Pass the entity to the LocationDao "insert(...)".
+    //  Call "getAllLocations()" in the end.
+    override suspend fun addNewAndGetUpdated(): List<Location> = withContext(Dispatchers.IO) {
+        val newLocationRequest = generateNewLocationRequest()
+        dataBase.locationsDao.insertLocation(toEntity(newLocationRequest))
+        getAllLocations()
+    }
+    // TODO 16: override suspend fun deleteByIdAndGetUpdated(id: Long).
+    //  Call this function "withContext(Dispatchers.IO)".
+    //  Call LocationDao "deleteById(id)" to delete location entry.
+    //  Call "getAllLocations()" in the end.
+
+    override suspend fun deleteByIdAndGetUpdated(id: Long): List<Location> =
+        withContext(Dispatchers.IO) {
+            dataBase.locationsDao.deleteLocationById(id)
+            getAllLocations()
+        }
+
+    private fun toEntity(location: NewLocationRequest) = Ws03LocationEntity(
+        title = location.title,
+        lat = location.latitude,
+        lon = location.longitude
+    )
+
+    private fun toLocation(entity: Ws03LocationEntity) = Location(
+        id = entity.id,
+        title = "${entity.title}, id:${entity.id}",
+        latitude = entity.lat,
+        longitude = entity.lon
+    )
+
+    private suspend fun generateNewLocationRequest() = withContext(Dispatchers.Default) {
+        delay(DELAY_MILLIS)
+        NewLocationRequest(
+            title = "Title ${random.nextInt()}",
+            latitude = random.nextDouble(180.0),
+            longitude = random.nextDouble(180.0)
+        )
+    }
+
+    companion object {
+        private const val DELAY_MILLIS: Long = 1_000
+    }
 }
